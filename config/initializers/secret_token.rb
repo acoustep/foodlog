69188b5756cc9646e9fa6e4a664801ff950b3caa
Mitch Stanley <mitch@fastmail.co.uk>
2014-03-22 17:45:38 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foodlog::Application.config.secret_key_base = 'eeca146759da6ab56c7dedc18d193e7af598e18ee525f752a94de10bd513aa1b9143e6f81b889962ca154c0b6e91156cfc4029373ef84df2ffd7e11f2bb3d40c'
